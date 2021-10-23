0813007ce2cc647a050bb3503809e85c01caf6a6
Jorge Lassance <lassancejorgeluiz@gmail.com>
2021-10-23 14:34:23 -0300

    Initial

---
+++ script.js
@@ -0,0 +1,9 @@
+//Transformando metodo em função para facilitar o uso
+const c = (el)=> document.querySelector(el);
+const cs = (el)=> document.querySelectorAll(el);
+
+pizzaJson.map((item, index)=>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+    c('.pizza-area').append(pizzaItem);
+});
