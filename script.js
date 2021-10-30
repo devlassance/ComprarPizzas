c1722befd622189b251993f050703e9834ad637c
Jorge Lassance <lassancejorgeluiz@gmail.com>
2021-10-30 17:18:06 -0300

    Adicionando informaçoes do carrinho em array

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 //Transformando metodo em função para facilitar o uso
 const c = (el)=> document.querySelector(el);
@@ -26,12 +28,15 @@ pizzaJson.map((item, index)=>{
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
 
-        //modalQt = 1;
+        modalQt = 1;
+       
 
         //closest busca pelo item mais proximo
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         //formatando preço
         let p = price(pizzaJson[key].price);
+
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -48,7 +53,7 @@ pizzaJson.map((item, index)=>{
             size.querySelector('span').innerHTML  = pizzaJson[key].sizes[indexSize];
         });
 
-        //c('pizzaInfo--qt').innerHTML = modalQt;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
         
 
         c('.pizzaWindowArea').style.opacity = 0;
@@ -60,4 +65,60 @@ pizzaJson.map((item, index)=>{
     });
 
     c('.pizza-area').append(pizzaItem);
+});
+
+//Eventos do Modal
+function closeModal(){
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+//fechando modal usando dois botões
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+})
+//pizzaInfo--qtmenos
+//pizzaInfo--qtmais
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+    if(modalQt > 1){    
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }   
+});
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+cs('.pizzaInfo--size').forEach((size, indexSize)=>{
+            
+    size.addEventListener('click', ()=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    }); 
+});
+
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+    
+    let identifier = pizzaJson[modalKey].id+'@'+size;
+
+    //percorrendo array até encontrar o item com identifier igual
+    let key = cart.findIndex((item)=>{
+        return item.identifier == identifier;
+    });
+
+    if(key > -1){   
+        cart[key].qt += modalQt;
+    }else{
+        cart.push({
+            identifier,
+            id:pizzaJson[modalKey].id,
+            size,
+            qt:modalQt
+        });
+    }
+    
+    closeModal();
 });
