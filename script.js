354a684f9e5c6d5495ef108ccefc8cf0e747e661
Jorge Lassance <lassancejorgeluiz@gmail.com>
2021-11-02 21:52:38 -0300

    Fix quantidade no carrinho

--- script.js
+++ script.js
@@ -145,53 +145,44 @@ function updateCart(){
         let desconto = 0;
         let total = 0;
 
-        cart.map((i) => {
-            let pizzaItem = pizzaJson.find((item)=>{
-                return item.id == i.id;
-            });
-
-            subtotal += pizzaItem.price * i.qt;
+        for(let i in cart) {
+            let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].qt;
 
             let cartItem = c('.models .cart--item').cloneNode(true);
 
             let pizzaSizeName;
-
-            switch(i.size){
+            switch(cart[i].size) {
                 case 0:
                     pizzaSizeName = 'P';
-                break;
-
+                    break;
                 case 1:
                     pizzaSizeName = 'M';
-                break;
-
+                    break;
                 case 2:
                     pizzaSizeName = 'G';
-                break;
+                    break;
             }
-
-            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
 
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
-            cartItem.querySelector('.cart--item--qt').innerHTML = i.qt;
-
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
             cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
-                if(i.qt > 1){
-                    i.qt--;
-                }else{
+                if(cart[i].qt > 1) {
+                    cart[i].qt--;
+                } else {
                     cart.splice(i, 1);
                 }
                 updateCart();
             });
-
             cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
-                i.qt++;
+                cart[i].qt++;
                 updateCart();
             });
 
             c('.cart').append(cartItem);
-        });
+        }
 
         desconto = subtotal * 0.1;
         total = subtotal - desconto;
